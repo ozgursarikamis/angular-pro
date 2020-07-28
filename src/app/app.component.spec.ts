920e65824fe9f4b8d49b48015e8cfe127591d5b8
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-07-28 19:49:55 +0300

    project rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-generic'`, () => {
+  it(`should have as title 'angular-pluralsight'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-generic');
+    expect(app.title).toEqual('angular-pluralsight');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-generic app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-pluralsight app is running!');
   });
 });
