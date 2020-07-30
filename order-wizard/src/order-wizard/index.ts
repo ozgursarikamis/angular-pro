f373629d76a6d46fd51c3d2f5d435318f5d4a7ad
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-07-31 01:58:22 +0300

    adding another filter function

--- order-wizard/src/order-wizard/index.ts
+++ order-wizard/src/order-wizard/index.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template, filter } from '@angular-devkit/schematics';
 import { normalize, strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -13,10 +13,20 @@ export function orderWizard(_options: any): Rule {
       template({
         ...strings,
         ..._options
-      })
+      }),
+      specFilter(_options)
     ]);
 
     const templateRute = mergeWith(newTree, MergeStrategy.Default);
     return templateRute(tree, _context);
   };
+}
+
+function specFilter(_options: any): Rule {
+  if (_options.spec === 'false') {
+    return filter(path => {
+      return !path.match(/\.spec\.ts$/) && !path.match(/test\.ts$/);
+    })
+  }
+  return filter(path => !path.match(/test\.ts$/))
 }
