ad49b19f7d07da193d9f2db697b4475c0b62c6c9
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-01 17:21:21 +0300

    generating module path

--- order-wizard/src/order-wizard/index.ts
+++ order-wizard/src/order-wizard/index.ts
@@ -1,11 +1,13 @@
-import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template, filter } from '@angular-devkit/schematics';
-import { normalize, strings } from '@angular-devkit/core';
+import { Rule, SchematicContext, Tree, url, apply, move, mergeWith, MergeStrategy, template, filter, SchematicsException, chain } from '@angular-devkit/schematics';
+import { normalize, strings, experimental } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function orderWizard(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const folderPath = normalize(_options.path + '/' + _options.name);
+
+    const workspace = getWorkspace(_options, tree);
     let files = url('./files');
 
     const newTree = apply(files, [
@@ -17,8 +19,11 @@ export function orderWizard(_options: any): Rule {
       specFilter(_options)
     ]);
 
-    const templateRute = mergeWith(newTree, MergeStrategy.Default);
-    return templateRute(tree, _context);
+    const templateRule = mergeWith(newTree, MergeStrategy.Default);
+    const updateModuleRule = updateRootModule(_options, workspace);
+
+    const chainedRule = chain([templateRule, updateModuleRule]);
+    return chainedRule;
   };
 }
 
@@ -29,4 +34,35 @@ function specFilter(_options: any): Rule {
     })
   }
   return filter(path => !path.match(/test\.ts$/))
+}
+
+function getWorkspace(_options:any, tree: Tree): experimental.workspace.WorkspaceSchema {
+  const workspace = tree.read('/angular.json');
+  if (!workspace) {
+    throw new SchematicsException('angular.json file not found');
+  }
+  return JSON.parse(workspace.toString());
+}
+
+function updateRootModule(_options:any, workspace: any) {
+  return (tree: Tree, _context: SchematicsContext): Tree => {
+    _options.project = (_options.project === 'defaultProject!') ? workspace.defaultProject : _options.project;
+
+    const project = workspace.projects[_options.project];
+    const moduleName = strings.dasherize(_options.name);
+    const exportedModuleName = strings.classify(_options.name);
+    const modulePath = strings.dasherize(_options.path);
+
+    const rootModulePath = `${project.root}/${project.sourceRoot}/${project.prefix}/${project.prefix}.module.ts`;
+
+    // generate import path:
+    const importContent = `import { ${exportedModuleName}Module } from './${modulePath}/${moduleName}/${moduleName}.module';`
+
+    // write import path to the file:
+    const rec = tree.beginUpdate(rootModulePath);
+    rec.insertLeft(0, importContent);
+    tree.commitUpdate(rec);
+
+    return tree;
+  }
 }
