53b4cacdd55c25c5c7d549cbea1a2c019f81a0f4
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-29 07:42:11 +0300

    import users module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ParentComponent } from "./parent/parent.component";
+import { ChildComponent } from "./child/child.component";
+import { UsersModule } from "./users/users.module";
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       ParentComponent,
       ChildComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
