9c3b26e9e8446d04531692cfcb539db2d129f645
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-05 00:54:14 +0300

    reset the form elements

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -14,6 +14,10 @@ export class StockSelectorComponent implements OnInit {
   @Output() added = new EventEmitter<any>();
   onAdd() {
     this.added.emit(this.parent.get('selector').value);
+    this.parent.get("selector").reset({
+      product_id: '',
+      quantity: 10
+    });
   }
   constructor() { }
 
