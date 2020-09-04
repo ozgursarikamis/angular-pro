3b69dab710bcf41fb9fae65381c25ebb4350a54b
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-05 00:59:35 +0300

    stock counter component

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -8,13 +8,15 @@ import { StockBranchComponent } from "./components/stock-branch/stock-branch.com
 import { StockSelectorComponent } from "./components/stock-selector/stock-selector.component";
 import { StockProductsComponent } from "./components/stock-products/stock-products.component";
 import { StockInventoryService } from "./services/stock-inventory.service";
+import { StockCounterComponent } from './components/stock-counter/stock-counter.component';
 
 @NgModule({
   declarations: [
     StockInventoryComponent,
     StockBranchComponent,
     StockSelectorComponent,
-    StockProductsComponent
+    StockProductsComponent,
+    StockCounterComponent
   ],
   imports: [
     CommonModule,
