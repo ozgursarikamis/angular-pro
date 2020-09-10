62723fdd3a5e50037e81ad6ae2f5a43ad1443cd9
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-10 22:48:36 +0300

    Custom preloading strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { Routes, RouterModule, PreloadingStrategy, Route } from "@angular/router";
+import { Observable, of } from 'rxjs';
 import { AdminRoutingModule } from "./admin/admin.routing.module";
 
+export class CustomPreload implements PreloadingStrategy {
+
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    // tell angular which route you want to preload:
+    return route.data && route.data.preload ? fn()
+      : of(null);
+  }
+}
 const routes: Routes = [
   { path: "", redirectTo: "/", pathMatch: "full" },
   {
-    path: "admin",
+    path: "admin", data: { preload: true },
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }), AdminRoutingModule],
+  providers: [CustomPreload],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: CustomPreload }), AdminRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
