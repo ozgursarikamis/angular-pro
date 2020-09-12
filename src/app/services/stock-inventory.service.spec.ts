b2fac12ac3b0057b3512823ba257eca99b142951
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-12 15:38:12 +0300

    testing http request

--- src/app/services/stock-inventory.service.spec.ts
+++ src/app/services/stock-inventory.service.spec.ts
@@ -2,10 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { StockInventoryService } from "./stock-inventory.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
-fdescribe('StockInventoryService', () => {
+describe('StockInventoryService', () => {
 
   let service: StockInventoryService;
-  let httpTestingController: HttpTestingController;
+  let httpMock: HttpTestingController;
+  const productList = [{"id":"1","price":"16.00","name":"Goody"},{"id":"2","price":"742.00","name":"Ergonomic Plastic Chair"},{"id":"3","price":"294.00","name":"Fantastic Granite Shoes"},{"id":"4","price":"260.00","name":"Small Rubber Chair"},{"id":"5","price":"740.00","name":"Ergonomic Cotton Hat"},{"id":"6","price":"203.00","name":"Unbranded Wooden Chair"},{"id":"7","price":"673.00","name":"Generic Wooden Soap"},{"id":"8","price":"376.00","name":"Intelligent Frozen Mouse"},{"id":"9","price":"425.00","name":"Practical Fresh Salad"},{"id":"10","price":"70.00","name":"Practical Plastic Car"},{"id":"11","price":"846.00","name":"Fantastic Cotton Car"},{"id":"12","price":"747.00","name":"Rustic Soft Sausages"},{"id":"13","price":"15.00","name":"Gorgeous Metal Soap"},{"id":"14","price":"64.00","name":"Rustic Steel Mouse"},{"id":"15","price":"495.00","name":"Licensed Concrete Salad"},{"id":"16","price":"800.00","name":"Incredible Metal Car"},{"id":"17","price":"826.00","name":"Refined Fresh Bacon"},{"id":"18","price":"925.00","name":"Gorgeous Rubber Sausages"},{"id":"19","price":"966.00","name":"Fantastic Rubber Chips"},{"id":"20","price":"198.00","name":"Incredible Cotton Fish"}];
+  const url = 'https://5f51071d5e98480016123523.mockapi.io';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,11 +17,22 @@ fdescribe('StockInventoryService', () => {
 
     // we inject our service (which imports the HttpClient)
     // and the Test Controller
-    httpTestingController = TestBed.inject(HttpTestingController);
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(StockInventoryService);
   });
 
+  it('should list products data', () => {
+    service.getProducts().subscribe(list => {
+      expect(list).toEqual(productList);
+    });
+
+    const req = httpMock.expectOne(`${url}/products`);
+    expect(req.request.method).toBe('GET');
+    // Note That we are flushing dummy "http" response
+    req.flush(productList);
+  });
+
   afterEach(() => {
-    httpTestingController.verify();
+    httpMock.verify();
   });
 });
