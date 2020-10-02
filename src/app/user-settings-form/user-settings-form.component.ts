7a33722c90a01815eefbf0ae6bdf7c616e424d7e
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-03 01:26:11 +0300

    UserSettings

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from "../data/user-settings";
 
 @Component({
-  selector: 'app-user-settings-form',
-  templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.scss']
+	selector: 'app-user-settings-form',
+	templateUrl: './user-settings-form.component.html',
+	styleUrls: ['./user-settings-form.component.scss']
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  constructor() { }
+	userSettings: UserSettings = {
+		emailOffers: true,
+		name: "Milton",
+		interfaceStyle: "dark",
+		subscriptionType: "annual",
+		notes: "some note"
+	}
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
