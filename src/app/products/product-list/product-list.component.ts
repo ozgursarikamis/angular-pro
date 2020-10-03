51a6de6a7c68f6f8c81f432b1365c8184f7405e6
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-03 18:34:08 +0300

    products component

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'pm-product-list',
+	templateUrl: './product-list.component.html',
+	styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+	pageTitle: string = 'Product List';
+	products: any[] = [
+		{
+			"productId": 2,
+			"productName": "Garden Cart",
+			"productCode": "GDN-0023",
+			"releaseDate": "March 18, 2019",
+			"description": "15 gallon capacity rolling garden cart",
+			"price": 32.99,
+			"starRating": 4.2,
+			"imageUrl": "assets/images/garden_cart.png"
+		},
+		{
+			"productId": 5,
+			"productName": "Hammer",
+			"productCode": "TBX-0048",
+			"releaseDate": "May 21, 2019",
+			"description": "Curved claw steel hammer",
+			"price": 8.9,
+			"starRating": 4.8,
+			"imageUrl": "assets/images/hammer.png"
+		}
+	];
+
+	constructor() { }
+	ngOnInit(): void { }
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'pm-products',
+	templateUrl: 'products.component.html'
+})
+
+export class ProductsComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit() { }
+}
