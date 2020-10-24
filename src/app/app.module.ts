c92c4571241d4ed0c54bff6d29418c98294f4738
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-25 00:41:50 +0300

    parent & child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BlueBackgroundDirective } from './shared/blue-background.directive';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlueBackgroundDirective
+    BlueBackgroundDirective,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
